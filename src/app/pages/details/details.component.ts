928537d0d14936fd9909115db5bae60a582ab7a0
Dener Troquatte <dener@vidafullstack.com.br>
2021-07-25 17:52:21 -0300

    Correções get public

--- src/app/pages/details/details.component.ts
+++ src/app/pages/details/details.component.ts
@@ -25,10 +25,10 @@ export class DetailsComponent implements OnInit {
   ) { }
 
   ngOnInit(): void {
-    this.getPokemon;
+    this.getPokemon();
   }
 
-  get getPokemon(){
+  public getPokemon(){
     const id = this.activatedRoute.snapshot.params['id'];
     const pokemon = this.pokeApiService.apiGetPokemon(`${this.urlPokemon}/${id}`);
     const name = this.pokeApiService.apiGetPokemon(`${this.urlName}/${id}`);
